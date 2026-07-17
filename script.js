489560bc8a4bed932f5a768adb1703c438d2632f
Shakhawat hosen <rahad744698@gmail.com>
2026-07-17 16:48:09 +0300

    defined new variable

--- script.js
+++ script.js
@@ -4,27 +4,24 @@ const file = document.querySelector("input");
 const btn = document.querySelector(".submit");
 
 pre.addEventListener("click", () => {
-    file.click();
+  file.click();
 });
 
-file.addEventListener("change", e => {
-    const file = e.target.files[0];
-    if (!file) {
-        pre.children[0].src = "./assets/uploadImage.png";
-        return;
-    }
-    const url = URL.createObjectURL(file);
-    pre.children[0].src = url;
+file.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  if (!file) {
+    pre.children[0].src = "./assets/uploadImage.png";
+    return;
+  }
+  const url = URL.createObjectURL(file);
+  pre.children[0].src = url;
 
-    btn.addEventListener("click", async () => {
-        document.querySelector(
-            "p"
-        ).innerText = `${location.host}/Newspaper/assets/${file.name}`;
+  btn.addEventListener("click", async () => {
+    const fileName = `${location.host}/Newspaper/assets/${file.name}`;
+    document.querySelector("p").innerText = fileName;
 
-        await navigator.clipboard
-            .writeText([`${location.host}/Newspaper/assets/${file.name}`])
-            .catch(err => {
-                console.log(err);
-            });
+    await navigator.clipboard.writeText([fileName]).catch((err) => {
+      console.log(err);
     });
+  });
 });
